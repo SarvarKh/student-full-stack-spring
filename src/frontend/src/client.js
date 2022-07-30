c9f045aff90cda9410caafa3a617ffae1f2f5cd8
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-07-30 11:45:23 +0500

    Implement getAllStudents

--- src/frontend/src/client.js
+++ src/frontend/src/client.js
@@ -1,2 +1,15 @@
 import 'unfetch/polyfill'
 
+const checkStatus = response => {
+    if (response.ok) {
+      return response;
+    }
+    // convert non-2xx HTTP responses into errors:
+    const error = new Error(response.statusText);
+    error.response = response;
+    return Promise.reject(error);
+}
+
+export const getAllStudents = () =>
+    fetch("localhost:8080/api/v2/customers")
+        .then(checkStatus);
