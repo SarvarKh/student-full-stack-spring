801a1330237fd55bfcc9db817ab45d7e2aac701b
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-08-03 06:51:23 +0500

    Update Student class and app.properties

--- src/main/java/com/example/studentdemo/student/Student.java
+++ src/main/java/com/example/studentdemo/student/Student.java
@@ -2,15 +2,34 @@ package com.example.studentdemo.student;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @ToString
 @Getter
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "student_sequence",
+            strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String email;
     private Gender gender;
+
+    public Student(String name, String email, Gender gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
 }
